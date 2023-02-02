99fa3f4b958198e824b0acd76387090bfe124c70
Jayesh SIngh <95732322+JAYESH262002@users.noreply.github.com>
2023-02-02 23:42:17 +0530

    Add files via upload

---
+++ R_Basic_Codes/Data Types in R_2.R
@@ -0,0 +1,16 @@
+x1<-7
+x2<-14
+
+x3<-x1+x2
+x3
+
+x4<-4L #stored Integer 4
+
+sqrt_var<-sqrt(x4) #Square root of 4
+sqrt_var
+
+#Appending (in R known as Paste)
+s1<-'Ram Ram'
+s2<-'Bhai'
+salutation<-paste(s1, s2) # extra space will be added at the end
+salutation
